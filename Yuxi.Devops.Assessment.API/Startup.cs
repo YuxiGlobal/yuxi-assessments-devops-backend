d1b8039c2d0e04f5529a02aa1553e1d5338e7965
Felipe Jaramillo <pipeluck@gmail.com>
2018-05-30 06:55:34 -0500

    include swagger

--- Yuxi.Devops.Assessment.API/Startup.cs
+++ Yuxi.Devops.Assessment.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.HealthChecks;
 using Yuxi.Devops.Assessment.Core.Repositories;
 using Yuxi.Devops.Assessment.Infrastructure.Persistence;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Yuxi.Devops.Assessment.API
 {
@@ -35,6 +36,12 @@ namespace Yuxi.Devops.Assessment.API
                     ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("Ok")));
             });
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
 
         }
 
@@ -45,6 +52,18 @@ namespace Yuxi.Devops.Assessment.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseMvc();
         }
